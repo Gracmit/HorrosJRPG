9a3fd8a228a02ebf5dfe6bcda0b78eba3fed090c
Gracmit <aleksii97@gmail.com>
2020-05-25 14:05:41 +0300

    Added Inventory and Item Pickup

---
+++ Horros/Assets/Scripts/Item&Inventory/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<Item> _items = new List<Item>();
+
+    public void PickUpItem(Item item)
+    {
+        _items.Add(item);
+    }
+}
---
+++ Horros/Assets/Scripts/Item&Inventory/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var inventory = other.GetComponent<Inventory>();
+        if (inventory != null)
+        {
+            inventory.PickUpItem(this);
+        }
+    }
+}
