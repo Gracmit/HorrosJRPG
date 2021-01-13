7e7819dde64a625f4109bb95b39f3d66715d826a
Gracmit <aleksii97@gmail.com>
2021-01-13 14:28:53 +0200

    Added StatusData Scriptable object, that saves data when transitioning between exploration and combat

---
+++ Horros/Assets/Scripts/ScriptableObjects/StatusData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StatusData", menuName = "Status/Data")]
+public class StatusData : ScriptableObject
+{
+    public PartyMember member;
+    public float[] position = new float[3];
+}
