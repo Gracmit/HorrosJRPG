18a3a932b0b5c381f467925ddeea77278640da40
Gracmit <aleksii97@gmail.com>
2021-04-09 16:59:05 +0300

    Added more tests for statusdata

--- Horros/Assets/PlayTests/statusdata.cs
+++ Horros/Assets/PlayTests/statusdata.cs
@@ -38,4 +38,28 @@ public class statusdata
         Assert.AreEqual(player.transform.position.y, statusManager.StatusData.position[1]);
         Assert.AreEqual(player.transform.position.z, statusManager.StatusData.position[2]);
     }
+
+    [Test]
+    public void can_resave_position_and_enemies()
+    {
+        var statusManager = new GameObject().AddComponent<StatusManager>();
+        statusManager.SetStatusData(ScriptableObject.CreateInstance<StatusData>());
+        
+        var enemyPool = new GameObject().AddComponent<EnemyPool>();
+        var enemy = ScriptableObject.CreateInstance<CombatEnemyData>();
+        var enemy2 = ScriptableObject.CreateInstance<CombatEnemyData>();
+        enemyPool.AddEnemy(enemy);
+        
+        var player = new GameObject();
+        player.transform.position = new Vector3(1, 2, 3);
+        statusManager.SetBattleData(player, enemyPool);
+        
+        enemyPool.AddEnemy(enemy2);
+        player.transform.position = new Vector3(4,5,6);
+        statusManager.SetBattleData(player, enemyPool);
+        Assert.AreEqual(player.transform.position.x, statusManager.StatusData.position[0]);
+        Assert.AreEqual(player.transform.position.y, statusManager.StatusData.position[1]);
+        Assert.AreEqual(player.transform.position.z, statusManager.StatusData.position[2]);
+        Assert.AreEqual(enemyPool.EnemyCount, statusManager.StatusData.enemyGroup.Count);
+    }
 }
