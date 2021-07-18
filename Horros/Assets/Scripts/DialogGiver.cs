ae046a0e2cc662f98320a72a3b96eec09d8bc86a
Gracmit <aleksii97@gmail.com>
2021-07-18 13:56:40 +0300

    Added controller to handle the integration of ink's texts to dialogbox

---
+++ Horros/Assets/Scripts/DialogGiver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogGiver : MonoBehaviour
+{
+    [SerializeField] private TextAsset _dialog;
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<PlayerMovementController>();
+        if (player != null)
+        {
+            FindObjectOfType<DialogController>().StartDialog(_dialog);
+            transform.LookAt(player.transform);
+        }
+    }
+}
---
+++ Horros/Assets/Scripts/UI/Dialog/DialogController.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using Ink.Runtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _storyText;
+    [SerializeField] private Button[] _choiceButtons;
+
+    private Story _story;
+
+    [ContextMenu("Start Dialog")]
+    public void StartDialog(TextAsset dialog)
+    {
+        _story = new Story(dialog.text);
+        RefreshView();
+    }
+
+    private void RefreshView()
+    {
+        StringBuilder storyTextBuilder = new StringBuilder();
+        while (_story.canContinue)
+        {
+            storyTextBuilder.AppendLine(_story.Continue());
+            HandleTags();
+        }
+
+        _storyText.SetText(storyTextBuilder);
+
+        for (int i = 0; i < _choiceButtons.Length; i++)
+        {
+            var button = _choiceButtons[i];
+            button.gameObject.SetActive(i < _story.currentChoices.Count);
+            button.onClick.RemoveAllListeners();
+            if (i < _story.currentChoices.Count)
+            {
+                var choice = _story.currentChoices[i];
+                button.GetComponentInChildren<TMP_Text>().SetText(choice.text);
+                button.onClick.AddListener(() =>
+                {
+                    _story.ChooseChoiceIndex(choice.index);
+                    RefreshView();
+                });
+            }
+        }
+    }
+
+    private void HandleTags()
+    {
+        foreach (var tag in _story.currentTags)
+        {
+            if (tag.StartsWith("E."))
+            {
+                var eventName = tag.Remove(0, 2);
+                //GameEvent.RaiseEvent(eventName);
+            }
+        }
+    }
+}
