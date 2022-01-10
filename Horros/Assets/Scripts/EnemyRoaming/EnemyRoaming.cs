63ef22960a1e989f53541dce3be06376e5e30107
Gracmit <aleksii97@gmail.com>
2022-01-10 18:58:26 +0200

    Simple overworld enemy roaming through predefined path

---
+++ Horros/Assets/Scripts/EnemyRoaming/EnemyRoaming.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyRoaming : MonoBehaviour
+{
+    [SerializeField] List<NavigationPoint> _navigationPoints;
+
+    int _index = 0;
+    NavMeshAgent _agent;
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.SetDestination(_navigationPoints[_index].transform.position);
+    }
+
+    void NextIndex()
+    {
+        if (_index >= _navigationPoints.Count - 1)
+            _index = 0;
+        else
+            _index++;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, _navigationPoints[_index].transform.position) < 1) 
+        {
+            NextDestination();
+        }
+    }
+
+
+    public void NextDestination()
+    {
+        NextIndex();
+        _agent.SetDestination(_navigationPoints[_index].transform.position);
+        _agent.isStopped = false;
+    }
+}
---
+++ Horros/Assets/Scripts/EnemyRoaming/NavigationPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class NavigationPoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            var roaming = other.GetComponent<EnemyRoaming>();
+            roaming.NextDestination();
+        }
+    }
+}
