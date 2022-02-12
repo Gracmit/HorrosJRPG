8b494be68f0655b3f7e1d60480e8f85911f8e889
Gracmit <aleksii97@gmail.com>
2022-02-12 11:38:27 +0200

    Added Treasure chest

---
+++ Horros/Assets/Scripts/Interactables/TreasureChest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TreasureChest : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Item[] _items;
+    public void Interact(GameObject player)
+    {
+        var inventory = FindObjectOfType<Inventory>();
+        foreach (var item in _items)
+        {
+            inventory.PickUpItem(item);
+            Debug.Log($"Picked up {item}");
+        }
+    }
+}
