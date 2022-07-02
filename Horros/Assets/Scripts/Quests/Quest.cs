7a25c3233869ce9d3fe6f9a7b85bf19bfd5c8f0e
Gracmit <aleksii97@gmail.com>
2022-07-02 18:46:26 +0300

    Started working on Quest system. Added base structure for the Quests that are scriptable objects

---
+++ Horros/Assets/Scripts/Quests/Quest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quest")]
+public class Quest : ScriptableObject
+{
+    [SerializeField] string _name;
+    [SerializeField] string _description;
+    [Tooltip("Designer/programmer notes, not visible to player")]
+    [SerializeField] string _notes;
+    public List<Step> Steps;
+}
+
+[Serializable]
+public class Step
+{
+    [SerializeField] string _instructions;
+    public List<Objectives> Objectives;
+}
+
+[Serializable]
+public class Objectives
+{
+    [SerializeField] ObjectiveType _objectiveType;
+    public enum ObjectiveType
+    {
+        Flag,
+        Item,
+        Kill,
+    }
+}
