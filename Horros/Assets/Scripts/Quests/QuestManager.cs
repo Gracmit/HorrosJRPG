97f37ac217ead860e33a96f3ba0ec52cb9b949f8
Gracmit <aleksii97@gmail.com>
2022-07-06 18:28:57 +0300

    Started working on Quest Manager

---
+++ Horros/Assets/Scripts/Quests/QuestManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    private List<Quest> _activeQuests = new List<Quest>();
+
+    public void AddQuest(Quest quest)
+    {
+        _activeQuests.Add(quest);
+    }
+}
