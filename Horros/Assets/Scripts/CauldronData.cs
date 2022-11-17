1d8de65c13a1479224b7d3047ad2b41ce42eb788
Gracmit <aleksii97@gmail.com>
2022-11-17 13:42:08 +0200

    SAving the state of the cauldron. Used Scriptable object. Need to expand to proper saving system

---
+++ Horros/Assets/Scripts/CauldronData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "CauldronData", fileName = "CauldronData", order = 0)]
+public class CauldronData : ScriptableObject
+{
+   public float BloodAmount;
+   public bool FirstAnimation = true;
+   public bool Done;
+}
